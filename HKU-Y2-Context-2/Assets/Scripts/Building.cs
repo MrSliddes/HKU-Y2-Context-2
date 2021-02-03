480d643130b575093ac5d3bd6ce81238684e534a
MrSliddes <mrsliddes@outlook.com>
2021-02-03 15:02:55 +0100

    File structure

---
+++ HKU-Y2-Context-2/Assets/Scripts/Building.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Building : MonoBehaviour
+{
+    public BuildingType buildingType;
+    public float buildingRange = 1;
+
+    public LayerMask layerMaskBuildings;
+    public TextMeshPro buildingScoreText;
+
+    public bool isPlaced = false;
+    public float buildingScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlaced) return;
+
+        buildingScore = 0; // reset
+
+        // Check surrounding buildings
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 1, layerMaskBuildings);
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            // Check if building
+            if(colliders[i].GetComponent<Building>() != null)
+            {
+                Building other = colliders[i].GetComponent<Building>();
+
+                // is building now get score
+                switch(buildingType)
+                {
+                    case BuildingType.house:
+                        switch(other.buildingType)
+                        {
+                            case BuildingType.house: buildingScore += 1; break;
+                            case BuildingType.centrale: buildingScore -= 2; break;
+                            case BuildingType.greenEnergy: buildingScore -= 1; break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case BuildingType.powerTower:
+                        break;
+                    case BuildingType.centrale:
+                        break;
+                    case BuildingType.greenEnergy:
+                        buildingScore += 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Update score
+        buildingScoreText.text = buildingScore.ToString();
+    }
+
+    public void Placed()
+    {
+        isPlaced = true;
+        buildingScoreText.text = "";
+    }
+}
+
+public enum BuildingType
+{
+    house,
+    powerTower,
+    centrale,
+    greenEnergy
+}
---
+++ HKU-Y2-Context-2/Assets/Scripts/Test_0.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_0 : MonoBehaviour
+{
+    public GameObject[] prefabBuildings;
+    public int currentBuildingIndex;
+
+    public LayerMask layerMaskDefault;
+
+    public Transform previewTransform;
+    public GameObject previewBuilding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previewBuilding = Instantiate(prefabBuildings[currentBuildingIndex]);
+        currentBuildingIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change building
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            currentBuildingIndex++;
+            if(currentBuildingIndex > prefabBuildings.Length - 1) currentBuildingIndex = 0;
+            GameObject a = previewBuilding;
+            previewBuilding = null;
+            Destroy(a);
+            previewBuilding = Instantiate(prefabBuildings[currentBuildingIndex]);
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            currentBuildingIndex--;
+            if(currentBuildingIndex < 0) currentBuildingIndex = prefabBuildings.Length - 1;
+            GameObject a = previewBuilding;
+            previewBuilding = null;
+            Destroy(a);
+            previewBuilding = Instantiate(prefabBuildings[currentBuildingIndex]);
+        }
+        
+
+        // Show review model
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMaskDefault))
+        {
+            // Do something with the object that was hit by the raycast.
+            previewTransform.position = hit.point;
+            previewBuilding.transform.position = hit.point;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit))
+            {
+                // Place current house
+                GameObject a = Instantiate(prefabBuildings[currentBuildingIndex], hit.point, Quaternion.identity);
+                a.GetComponent<Building>().Placed();
+            }
+        }
+    }
+}
