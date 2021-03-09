45ef3d4a4e048c102e9173cc12ed9a6e2c3088a9
MrSliddes <mrsliddes@outlook.com>
2021-03-09 20:19:15 +0100

    Update
    ## Added
    -End and Start gameobjects
    - level transition stuff, not yet working

---
+++ HKU-Y2-Context-2/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public GameObject[] objectsToHideOnStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject item in objectsToHideOnStart)
+        {
+            item.SetActive(false);
+        }
+
+        if(sceneToLoad == "")
+        {
+            Debug.LogError("No scene name set! Please set a scene name to load");
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
---
+++ HKU-Y2-Context-2/Assets/Scripts/LevelStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get
+
+        // Set
+        // Check if there are more than 1
+        if(FindObjectsOfType<LevelStart>().Length > 1)
+        {
+            Debug.LogError("There are multiple Level Start Objects, there should only be one!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            FindObjectOfType<Player>().transform.position = transform.position;
+            Destroy(gameObject);
+        }    
+    }
+}
