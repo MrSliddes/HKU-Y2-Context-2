09d86b3789b143e0661246421bc57623455fb5e7
MrSliddes <mrsliddes@outlook.com>
2021-03-12 00:18:49 +0100

    Update
    Added
    - Stuff for demo 13/03
    - boomers
    - main menu
    - music

---
+++ HKU-Y2-Context-2/Assets/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+    public string sceneToLoadName;
+
+    public GameObject LoadingScreen;
+
+
+    public void Load()
+    {
+        LoadingScreen?.SetActive(true);
+        SceneManager.LoadSceneAsync(sceneToLoadName, LoadSceneMode.Single);
+    }
+}
